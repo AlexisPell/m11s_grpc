1c84ff76931fd5d92ceeb20e695d3050ee98df38
Aleksey Pogorelyy <apogorelyi@freetechs.org>
2023-11-19 03:37:43 +0500

    cfg + logger

---
+++ services/grpc-auth-service/cmd/sso/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/AlexisPell/m11s_grpc/services/grpc-auth-service/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// get config
+	cfg := config.MustLoad()
+
+	// logger
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting application", slog.Any("cfg", cfg))
+
+	// app initialization
+
+	// launch grpc-server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
+}
---
+++ services/grpc-auth-service/go.mod
@@ -0,0 +1,12 @@
+module github.com/AlexisPell/m11s_grpc/services/grpc-auth-service
+
+go 1.21.0
+
+require (
+	github.com/AlexisPell/m11s_grpc/protos v0.0.0-20231118213301-4f9e49a73c13 // indirect
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
